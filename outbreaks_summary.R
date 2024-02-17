48e1fbfe76bec5fd5c517b3a74eb346dd4a3798e
‘NataliaCiria’ <nataliaciria@hotmail.com>
2024-02-17 14:36:33 +0100

    add species to summary

--- outbreaks_summary.R
+++ outbreaks_summary.R
@@ -7,7 +7,8 @@ outbreaks<-read.csv("outbreaks.csv")
 outbreaks_summary<-outbreaks%>%
   mutate(year = substr(confirmation_date, start = 1, stop = 4)) %>%
   group_by(disease, year) %>%
-  summarise(outbreaks = n())
+  summarise(outbreaks = n(),
+            species=toString(unique(species)))
 
 write.csv(outbreaks_summary, "outbreaks_summary.csv", fileEncoding = "UTF-8")
 
