aea1d0f4979485195d52d19d6bf238dbfbdbb19f
‘NataliaCiria’ <nataliaciria@hotmail.com>
2024-02-17 14:19:16 +0100

    unique_outbreaks

--- outbreaks_summary.R
+++ outbreaks_summary.R
@@ -7,6 +7,7 @@ outbreaks<-read.csv("outbreaks.csv")
 outbreaks_summary<-outbreaks%>%
   mutate(year = substr(confirmation_date, start = 1, stop = 4)) %>%
   group_by(disease, year) %>%
-  summarise(outbreaks = n())
+  summarise(outbreaks = n(),
+            unique_outbreaks = n_distinct(code))
 
 write.csv(outbreaks_summary, "outbreaks_summary.csv", fileEncoding = "UTF-8")
