92cbf979e678fe4ba50cfa4f5ed630958f7839de
‘NataliaCiria’ <nataliaciria@hotmail.com>
2024-02-20 17:05:38 +0100

    Add animal species in English

--- outbreaks_summary.R
+++ outbreaks_summary.R
@@ -3,13 +3,21 @@ library(tidyr) #Data cleaning
 
 #load data
 outbreaks<-read.csv("outbreaks.csv")
+animals<-read.csv("animals.csv")
 
 outbreaks_summary<-outbreaks%>%
   mutate(year = substr(confirmation_date, start = 1, stop = 4)) %>%
   group_by(disease, disease_label,year) %>%
-  summarise(outbreaks = n(),
-            species=toString(unique(species)))%>%
-  ungroup()%>%
+  summarise(outbreaks = n())%>%
+  ungroup()
+
+outbreaks_summary<-animals%>%
+  left_join(outbreaks, by="outbreak_id")%>%
+  mutate(year = substr(confirmation_date, start = 1, stop = 4)) %>%
+    group_by(disease, disease_label,year) %>%
+    summarise(species=toString(unique(species.x)))%>%
+    ungroup()%>%
+  left_join(outbreaks_summary)%>%
   arrange(desc(outbreaks))
 
 write.csv(outbreaks_summary, "outbreaks_summary.csv", fileEncoding = "UTF-8")
